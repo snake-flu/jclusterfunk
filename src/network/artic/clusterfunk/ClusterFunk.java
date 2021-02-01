1330b86aca64a24a2180e63e106b75f871862ddb
rambaut <rambaut@gmail.com>
2021-02-01 12:13:40 +0000

    Updating version

--- src/network/artic/clusterfunk/ClusterFunk.java
+++ src/network/artic/clusterfunk/ClusterFunk.java
@@ -14,7 +14,7 @@ import static network.artic.clusterfunk.ClusterFunkOptions.*;
 class ClusterFunk {
 
     private final static String NAME = "jclusterfunk";
-    private static final String VERSION = "v0.0.16";
+    private static final String VERSION = "v0.0.17";
     private static final String HEADER = NAME + " " + VERSION + "\nBunch of functions for trees\n\n";
     private static final String FOOTER = "";
 
