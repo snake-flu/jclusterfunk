a287fd7ef92289a2e0246f5b5719da1e5634856d
Andrew Rambaut <rambaut@gmail.com>
2021-05-11 13:52:59 +0100

    Finishing merge function

--- src/network/artic/clusterfunk/commands/Merge.java
+++ src/network/artic/clusterfunk/commands/Merge.java
@@ -1,8 +1,5 @@
 package network.artic.clusterfunk.commands;
 
-import jebl.evolution.graphs.Node;
-import jebl.evolution.taxa.Taxon;
-import jebl.evolution.trees.RootedTree;
 import org.apache.commons.csv.CSVRecord;
 
 import java.io.IOException;
@@ -29,30 +26,32 @@ public class Merge extends Command {
 
         super(isVerbose);
 
-        Map<String, CSVRecord> metadata1 = readCSV(metadataFileName1, indexColumn);
-        CSVRecord headerRecord1 = headerRecord;
+        Map<String, CSVRecord> metadata1 = readCSV(metadataFileName1, null);
+        List<String> headerNames1 = headerRecord.getParser().getHeaderNames();
         Map<String, CSVRecord> metadata2 = readCSV(metadataFileName2, indexColumn);
-        CSVRecord headerRecord2 = headerRecord;
+        List<String> headerNames2 = headerRecord.getParser().getHeaderNames();
+
+        indexColumn = (indexColumn == null ? headerNames2.get(0) : indexColumn);
+
+        if (!headerNames1.contains(indexColumn)) {
+            errorStream.println("Metadata file, " + metadataFileName1 + ", does not contain index column, " + indexColumn);
+            System.exit(1);
+        }
 
         if (isVerbose) {
             outStream.println("Read metadata table 1: " + metadataFileName1);
             outStream.println("                 Rows: " + metadata1.size());
-            outStream.println("         Index column: " + (indexColumn == null ? headerRecord1.getParser().getHeaderNames().get(0) : indexColumn));
             outStream.println();
-            outStream.println("Read metadata table 2: " + metadataFileName1);
-            outStream.println("                 Rows: " + metadata1.size());
-            outStream.println("         Index column: " + (indexColumn == null ? headerRecord1.getParser().getHeaderNames().get(0) : indexColumn));
+            outStream.println("Read metadata table 2: " + metadataFileName2);
+            outStream.println("                 Rows: " + metadata2.size());
+            outStream.println("         Index column: " + indexColumn);
             outStream.println();
         }
 
-        List<CSVRecord> metadataRows = new ArrayList<>();
-        for (String key : metadata2.keySet()) {
-            CSVRecord record1 = metadata1.get(key);
-            if (record1 != null) {
-                CSVRecord record2 = metadata2.get(key);
-
-//                new CSVRecord();
-//                metadataRows.add(metadata.get(taxonMap.get(taxon)));
+        List<String> headerNames = new ArrayList<>(headerNames1);
+        for (String name : headerNames2) {
+            if (!headerNames1.contains(name)) {
+                headerNames.add(name);
             }
         }
 
@@ -61,118 +60,59 @@ public class Merge extends Command {
                 outStream.println("Writing merged metadata file, " + outputFileName);
                 outStream.println();
             }
-            writeMetadataFile(metadataRows, outputFileName);
+            int rowCount = 0;
+            int valueCount = 0;
 
+            boolean extractMatches = true;
             try {
                 PrintWriter writer = new PrintWriter(Files.newBufferedWriter(Paths.get(outputFileName)));
 
-                List<String> headerNames = metadata1.get(0).getParser().getHeaderNames();
                 writer.println(String.join(",", headerNames));
 
-//                for (CSVRecord record : records) {
-//                    CSVRecord record1 = metadata1.get(key);
-//
-//                    boolean first = true;
-//                    for (String value : record) {
-//                        if (first) {
-//                            writer.print(value);
-//                            first = false;
-//                        } else {
-//                            writer.print(",");
-//                            writer.print(value);
-//                        }
-//                    }
-//                    writer.println();
-//                }
+                for (String key : metadata1.keySet()) {
+                    CSVRecord record1 = metadata1.get(key);
+                    String index = record1.get(indexColumn);
+                    CSVRecord record2 = metadata2.get(index);
+                    if (record2 != null) {
+                        rowCount += 1;
+                    }
+                    if (!extractMatches || record2 != null) {
+                        boolean first = true;
+                        for (String name : headerNames) {
+                            String value = "";
+                            if (headerNames1.contains(name)) {
+                                value = record1.get(name);
+                            }
+                            if (record2 != null && (value.isEmpty() || overwriteExisting) && !name.equals(indexColumn)) {
+                                if (headerNames2.contains(name)) {
+                                    value = record2.get(name);
+                                    valueCount += 1;
+                                }
+                            }
+                            writer.print(first ? "" : ",");
+                            writer.print(value);
+                            first = false;
+                        }
+                        writer.println();
+                    }
+                }
 
                 writer.close();
             } catch (IOException e) {
                 errorStream.println("Error writing metadata file: " + e.getMessage());
                 System.exit(1);
             }
-        }
 
-    }
-
-    /**
-     * Annotates the tips of a tree with a set of columns from the metadata table
-     * @param tree
-     * @param taxonMap
-     * @param metadata
-     * @param columnNames
-     * @param replace
-     */
-    private void annotateTips(RootedTree tree,
-                              Map<Taxon, String> taxonMap,
-                              Map<String, CSVRecord> metadata,
-                              String[] columnNames,
-                              boolean replace,
-                              boolean ignoreMissing) {
-        if (replace) {
-            clearExternalAttributes(tree);
-        }
-
-        for (Node tip : tree.getExternalNodes()) {
-            String key = taxonMap.get(tree.getTaxon(tip));
-            CSVRecord record = metadata.get(key);
-            if (record == null) {
-                if (!ignoreMissing) {
-                    errorStream.println("Tip index, " + key + ", not found in metadata table");
-                    System.exit(1);
-                }
-            } else {
-                for (String name : columnNames) {
-                    if (!record.get(name).isEmpty()) {
-                        tip.setAttribute(name, record.get(name));
-                    }
-                }
+            if (isVerbose) {
+                outStream.println("Columns added: " + (headerNames.size() - headerNames1.size()));
+                outStream.println("  Rows merged: " + rowCount);
+                outStream.println("  Values merged: " + valueCount);
+                outStream.println();
             }
+
         }
+
     }
-
-    /**
-     * Annotates the tips of a tree with a set of columns from the metadata table
-     * @param tree
-     * @param taxonMap
-     * @param metadata
-     * @param columnNames
-     * @param replace
-     */
-    private void relabelTips(RootedTree tree,
-                             Map<Taxon, String> taxonMap,
-                             Map<String, CSVRecord> metadata,
-                             String[] columnNames,
-                             String headerDelimiter,
-                             boolean replace,
-                             boolean ignoreMissing) {
-        for (Node tip : tree.getExternalNodes()) {
-            String key = taxonMap.get(tree.getTaxon(tip));
-            CSVRecord record = metadata.get(key);
-            if (record == null) {
-                if (!ignoreMissing) {
-                    errorStream.println("Tip index, " + key + ", not found in metadata table");
-                    System.exit(1);
-                }
-            } else {
-                StringBuilder tipLabel = new StringBuilder();
-                boolean first = true;
-                if (!replace) {
-                    tipLabel.append(tree.getTaxon(tip).getName());
-                    first = false;
-                }
-
-                for (String name : columnNames) {
-                    if (!first) {
-                        tipLabel.append(headerDelimiter);
-                        first = false;
-                    }
-                    tipLabel.append(record.get(name));
-                }
-                tree.renameTaxa(tree.getTaxon(tip), Taxon.getTaxon(tipLabel.toString()));
-            }
-        }
-    }
-
 
 }
 
